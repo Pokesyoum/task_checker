9b4cee84e9e09fa5e304043e655c1b92d39c8832
Pokesyoum <syoumpm@gmail.com>
2025-02-10 14:49:07 +0900

    ヘッダーの作成

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./style.css"
+import { Header } from "../../components/header/index.tsx"; 
 
 export const Home = () => {
-  return <div className="main"></div>;
+  return <div className="main">
+    <Header></Header>
+  </div>;
 };
