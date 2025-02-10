52d03289e97371a03cdd9bc5691902703844b8ad
Pokesyoum <syoumpm@gmail.com>
2025-02-10 21:51:15 +0900

    ジャンル選択部分のマークアップ

---
+++ src/components/select/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css"
+
+export const Select = () => {
+  return (
+    <div>
+      <select className="select">
+        <option value={0}>0</option>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+      </select>
+    </div>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./style.css"
 import { Header } from "../../components/header/index.tsx"; 
+import { Select } from "../../components/select/index.tsx";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export const Home = () => {
   return <div className="main">
-    <Header></Header>
+    <Header />
+    <div className="genre">
+      <Select />
+      <AddCircleOutlineIcon 
+        className="add_circle_outline_icon"
+        font-size="medium"
+      />
+    </div>
   </div>;
 };
