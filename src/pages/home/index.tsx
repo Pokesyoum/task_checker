2d2beab67670b7d1f43024a0159acacbad0d2f13
Pokesyoum <syoumpm@gmail.com>
2025-02-12 20:54:11 +0900

    タスク一覧部分のマークアップ

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css"
 import { Header } from "../../components/header/index.tsx"; 
 import { Select } from "../../components/select/index.tsx";
+import { ToDoList } from "../../components/toDoList/index.tsx";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export const Home = () => {
@@ -13,6 +14,9 @@ export const Home = () => {
         className="add_circle_outline_icon"
         font-size="medium"
       />
+    </div>
+    <div className="contents">
+      <ToDoList />
     </div>
   </div>;
 };
