0bb42e34d4e9b9918c1faee80b039801ca5f5e62
Pokesyoum <syoumpm@gmail.com>
2025-02-23 17:46:34 +0900

    ディレクトリの修正

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/header/index.tsx";
 import { Select } from "../../components/select/index.tsx";
 import { ToDoList } from "../../components/toDoList/index.tsx";
 import { FormModal } from "../../components/modal/index.tsx";
-import { taskRequest } from "../../components/modal/taskRequest.ts";
+import { taskRequest } from "../../requests/taskRequest.ts";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 export const Home = () => {
